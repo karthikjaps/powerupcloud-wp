b8e8e032d58354685d459beb73dd1a0d234bcf81
Jamsheer Kandoth <jamsheer@yougotagift.com>
2017-09-25 06:47:14 +0400

    - Jenkins AWS Configure

--- jenkins_userdata.sh
+++ jenkins_userdata.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
 JENKINS_HOME=/opt/jenkins_home
-if [ ! -e /opt/jenkins_home/aws/config  ]; then
-    config="[default]
-            region = ${region}"
-    echo "$config" | tee /opt/jenkins_home/aws/config
-fi
-if [ ! -e /opt/jenkins_home/aws/credentials  ]; then
-    credentials="[default]
-        aws_access_key_id = ${aws_access_key}
-        aws_secret_access_key = ${aws_secret_key}"
-    echo "$credentials" | tee /opt/jenkins_home/aws/credentials
-fi
+config="[default]\nregion = ${region}"
+echo -e "$config" > /opt/jenkins_home/aws/config
+credentials="[default]\naws_access_key_id = ${aws_access_key}\naws_secret_access_key = ${aws_secret_key}"
+echo -e "$credentials" > /opt/jenkins_home/aws/credentials
 # Create and set correct permissions for Jenkins mount directory
 sudo mkdir -p $JENKINS_HOME
 sudo chmod -R 777 $JENKINS_HOME
