c50e30f4c3a549c74614fbda711478cba97652d7
Jamsheer Kandoth <jamsheer@yougotagift.com>
2017-09-25 04:59:01 +0400

    - AWS Cli with Jenkins Image

--- jenkins_userdata.sh
+++ jenkins_userdata.sh
@@ -6,5 +6,4 @@ JENKINS_HOME=/opt/jenkins_home
 sudo mkdir -p $JENKINS_HOME
 sudo chmod -R 777 $JENKINS_HOME
 # Start Jenkins
-docker run -u root -id --name jenkins3 -p 80:8080 -p 50000:50000 -v
-$JENKINS_HOME:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/bin/docker jamsheer/awscli-jenkins:latest
+docker run -u root -id --name jenkins3 -p 80:8080 -p 50000:50000 -v $JENKINS_HOME:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/bin/docker jamsheer/awscli-jenkins:latest
