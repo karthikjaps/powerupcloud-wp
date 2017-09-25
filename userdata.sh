74f84023a2f0b2a9eeaaa3fce3b5caf1cd75df67
Jamsheer Kandoth <jamsheer@yougotagift.com>
2017-09-25 21:16:13 +0400

    - Code Deploy Agent

--- userdata.sh
+++ userdata.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+sudo yum update
 yum install -y nfs-utils
+sudo yum install -y ruby
+sudo yum install -y wget
+cd /home/ec2-user
+wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
 echo "${nfs_fqdn}:/ /mnt/ nfs4  defaults  0   0" >> /etc/fstab
 mount -a
 service docker restart
