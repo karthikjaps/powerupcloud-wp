8e037c13405057ef479c41be4d16e9c4e7903682
Jamsheer Kandoth <jamsheer@yougotagift.com>
2017-09-25 04:40:44 +0400

    - AWS Cli with Jenkins Image

--- jenkins_userdata.sh
+++ jenkins_userdata.sh
@@ -5,7 +5,6 @@ JENKINS_HOME=/opt/jenkins_home
 # Create and set correct permissions for Jenkins mount directory
 sudo mkdir -p $JENKINS_HOME
 sudo chmod -R 777 $JENKINS_HOME
-curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | sudo python
-sudo env 'PATH=$PATH:/usr/local/bin' pip install awscli
 # Start Jenkins
-docker run -u root -id --name jenkins3 -p 80:8080 -p 50000:50000 -v $JENKINS_HOME:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/bin/docker jenkins/jenkins:lts
+docker run -u root -id --name jenkins3 -p 80:8080 -p 50000:50000 -v
+$JENKINS_HOME:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/bin/docker jamsheer/awscli-jenkins:latest
